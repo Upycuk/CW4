05604e2a9d673f34eb58249ec11ef58270ff60e0
Upycuk <iraida.com@yandex.ru>
2024-07-12 20:03:37 +0300

    Вакансии

---
+++ main.py
@@ -0,0 +1,23 @@
+import logging
+
+from src.api import HhAPI
+from src.exceptions import APIException
+from src.vacancy import Vacancy
+
+logger = logging.getLogger(__name__)
+
+def main():
+    hh = HhAPI()
+    hh.text = input('Введите запрос\n')
+    try:
+        data = hh.get_response_data()
+    except APIException as a:
+        logger.exception(f'Ошибка обращения к HHAPI. {a}')
+        print('Что-то пошло не так :(\nПриносим наши извинения.')
+
+    hh_vacancy = Vacancy('Гоша', 50000, 'url', 'employer')
+
+
+
+if __name__ == '__main__':
+    main()
